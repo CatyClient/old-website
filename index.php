86134e18df2f5811286fa8a43e541cd4de35c76e
cedkeChat01 <123301168+00cedke@users.noreply.github.com>
2024-04-20 16:08:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,115 @@
+<?php
 
+?>
+
+<html>
+        
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5, user-scalable=yes">
+    
+    <title>CatyNetwork V2.4</title>
+    
+    <meta name="description" content="CatyNetwork V2.4">
+    <meta name="keywords" content="Wii U Free Cheats Menus.">
+    <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1">
+    <meta name="og:description" content="version 1.">
+    <meta property="og:locale" content="en_US">
+    <meta property="og:type" content="article">
+    <meta property="og:title" content="CatyNetwork V2.4 Wii U">
+    <meta property="og:url" content="http://catymc.glitch.me">
+    <meta property="og:site_name" content="Made by cedkeChat and Azaee.">
+    <meta name="twitter:card" content="summary_large_image">
+
+    <!-- Links -->
+
+    <link rel="icon" href="fdp.ico">
+    <link rel="icon" href="fdp.png">
+    <link rel="shortcut icon" href="fdp.png">
+    <link rel="apple-touch-icon" href="fdp.png">
+    <link rel="stylesheet" href="dl.css">
+    <link rel="stylesheet" href="layout.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin="">
+    <link href="css2" rel="stylesheet">
+        <div class="top-bar">
+          <div style="float: left; margin-top: 16px; z-index: 2; position: absolute;">
+            <a href="index.html" style="text-decoration: none;">
+              <h2>CatyNetwork V2.4                                Made by cedkeChat01</h2>
+            </a>
+          </div>
+        </div>
+<br></br>
+<br></br>
+<br></br>
+
+<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+  </head>
+<div id="main-container">
+            <aside class="sidebar" style="display: none;"></aside>
+            <div id="section-container">
+              <section class="grid-container">
+
+
+                  <div class="grid-item preview-img" style="display: inline-block; margin: 0; text-align: right; background-color: #00000000; border-radius: 16px 0 0 16px; background-size: cover; background-position: 50% 50%; background-image:
+                    url('minecraft.jpg');">
+                  </div>
+                      
+                      
+                  <div class="grid-item preview-box" style="color:#376EED; ertical-align: top; display: inline-block; margin: 0; text-align: left; border-radius: 0 16px 16px 0;">
+                    CatyClient V3.2
+         <br></br>
+                    <h5 style="color:#37D1ED; font-weight: 200; flex-grow: 1;">
+                      Minecraft Wii U Cheats Free Made By cedkeChat01. (Server end in 2025).
+                    </h5
+                    <br>
+                    <div>
+                      <button class="glow-on-hover" style=" color:white; font-size:.9em; font-weight:300; align-self: end; margin: auto;" onclick="window.location.href='index-hax.php';">
+                        Start Mods
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                   <!-- Section -->
+        
+        
+        <br></br>
+        
+        
+        <head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+  </head>
+<div id="main-container">
+            <aside class="sidebar" style="display: none;"></aside>
+            <div id="section-container">
+              <section class="grid-container">
+                      
+     
+                    <div class="grid-item preview-img" style="display: inline-block; margin: 0; text-align: right; background-color: #00000000; border-radius: 16px 0 0 16px; background-size: cover; background-position: 50% 50%; background-image:
+                    url('minecraft.jpg');">
+                  </div>
+                      
+                      
+                  <div class="grid-item preview-box" style="color:#376EED; ertical-align: top; display: inline-block; margin: 0; text-align: left; border-radius: 0 16px 16px 0;">
+                    CatyClient V4
+         <br></br>
+                    <h5 style="color:#37D1ED; font-weight: 200; flex-grow: 1;">
+                      lol.
+                    </h5
+                    <br>
+                    <div>
+                      <button class="glow-on-hover" style=" color:white; font-size:.9em; font-weight:300; align-self: end; margin: auto;" onclick="window.location.href='index-hax.php';">
+                        Start New Mods
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                   <!-- Section -->
+        
+        
+        <br></br>
+                   
+                </div>
+            </div>
+          </div>
+     </html>
