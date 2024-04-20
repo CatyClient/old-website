8e6ee5f1ef27861b744ce1e2ddeeef594d9e5944
cedkeChat01 <123301168+00cedke@users.noreply.github.com>
2024-04-20 16:17:09 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,22 @@
 <?php
 
-phpinfo();
-
+$is_true = false; 
+$work = false;
+  
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test</title>
+</head>
+<body>
+    <?php if ($is_true): ?>
+        <?php work = true; ?>
+    <?php else: ?>
+        <?php work = false; ?>
+    <?php endif; ?>
+</body>
+</html>
+
